5b8cf812b4f2a15dcce36c1d48b40019874482fb
Islam Mohamed <islamm34@gmail.com>
2025-12-21 13:41:02 +0200

    project clean up

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp();
   }
 }
